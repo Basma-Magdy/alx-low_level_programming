7ee0d1a3f20191ce3e1d2fbb759293c21ac3b130
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-04-20 18:27:27 -0700

    0x00 C Hello World Tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - entry point
+ * return (0) - always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
