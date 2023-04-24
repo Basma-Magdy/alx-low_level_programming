b97f44935098ac0fe88fed4f802773d1ba424997
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-04-24 09:28:22 -0700

    0x01 C Variables, if, else, while Tasks

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,2 +1,16 @@
-ffhhf
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 success
+ */
+int main(void)
+{
+	char ch;
 
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	return(0);
+}
