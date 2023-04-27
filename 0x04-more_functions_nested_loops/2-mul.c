44d02768e4f752cb21ebe9a4edfd8e56405a95dc
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-04-27 16:13:05 -0700

    0x03. C - Debugging

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul -prints the sign of a number
+* Return: 1,0,-1
+* @a: character of ascii code
+* @b: character of ascii code
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
