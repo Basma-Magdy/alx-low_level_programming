d70d81eee74514f2f596a4ac51aba52d4ec451dc
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-04-27 16:25:29 -0700

    0x03. C - Debugging

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_most_numbers -prints the sign of a number
+* Return: 1,0,-1
+*/
+
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+if (i == 50 || i == 52)
+{
+}
+else
+{
+_putchar (i);
+}
+_putchar ('\n');
+}
