baff5d818c42894404e6a71ca147e6930b9a9d17
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-04-27 16:46:26 -0700

    0x03. C - Debugging

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_line -prints the sign of a number
+* Return: 1,0,-1
+* @n: character of ascii code
+*/
+void print_line(int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
