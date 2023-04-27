a75c34966f4bb4b039f2cfeb7dccacfecab27ee7
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-04-26 18:46:42 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,15 +6,24 @@
 void jack_bauer(void)
 {
 
-char i, j;
+short i, j;
+char high_byte_hrs, low_byte_hrs, high_byte_mins, low_byte_mins;
 
 for (i = 0; i <= 23; i++)
 {
+	low_byte_hrs = i && 0x0f;
+	high_byte_hrs = (i && 0xf0) >> 4;
+
 	for (j = 0; j <= 59; j++)
 	{
-		_putchar(i);
+		low_byte_mins = j && 0x0f;
+		high_byte_mins = (j && 0xf0) >> 4;
+
+		_putchar(low_byte_hrs);
+		_putchar(high_byte_hrs);
 		_putchar(':');
-		_putchar(j);
+		_putchar(low_byte_mins);
+		_putchar(high_byte_mins);
 		_putchar('\n');
 	}
 }
