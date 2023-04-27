f1f0ee91c41c35960c5fabbe43b93f395b2b7043
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-04-27 16:53:55 -0700

    0x03. C - Debugging

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_square -prints the sign of a number
+* Return: 1,0,-1
+* @size: character of ascii code
+*/
+void print_square(int size)
+{
+int col, row;
+
+for (col = 0; col < size; col++)
+{
+for (row = 0; row < size; row++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+_putchar('\n');
+}
