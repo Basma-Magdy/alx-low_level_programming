1a5896cab6883cd681e4305904488bf3dfc49850
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-04-26 19:27:50 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* add -prints the sign of a number
+* Return: 1,0,-1
+* @x: character of ascii code
+* @y: character
+*/
+int add(int x, int y)
+{
+int result;
+
+result = x + y;
+
+return (result);
+
+}
