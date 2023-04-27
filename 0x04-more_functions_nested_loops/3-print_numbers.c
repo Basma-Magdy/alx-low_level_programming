f3b12e1b84eb4c66da263b9a4a80fc9f5b753771
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-04-27 16:18:39 -0700

    0x03. C - Debugging

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers -prints the sign of a number
+* Return: 1,0,-1
+*/
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+_putchar (i);
+}
+_putchar ('\n');
+}
