9fcb32686275122c91864d9c1d5014be7aa00e3c
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-04-27 17:01:21 -0700

    0x03. C - Debugging

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,7 +8,7 @@ void print_square(int size)
 {
 int col, row;
 
-for (col = 0; col <= size; col++)
+for (col = 0; col < size; col++)
 {
 for (row = 0; row < size; row++)
 {
@@ -16,4 +16,7 @@ _putchar('#');
 }
 _putchar('\n');
 }
+
+if (size == 0)
+_putchar('\n');
 }
