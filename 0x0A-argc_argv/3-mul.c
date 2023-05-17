6fd2a5825d07f9e6b3e9985307312a5897712241
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-17 14:43:15 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: always 0 success
+*@argc: number of commands
+* @argv: array of pointers to commands
+*/
+int main(int argc, char const *argv[])
+{
+int mul;
+
+if (argc - 1 == 0)
+{
+printf("Error \n");
+return (1);
+}
+
+else
+{
+mul = (*argv[argc - 1] - 48) * (*argv[argc - 2] - 48);
+printf("%d\n", mul);
+}
+return (0);
+}
+
