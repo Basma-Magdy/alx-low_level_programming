b9235cf889e9bf5010301918765f363fcf68469d
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-17 12:11:34 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Return: always 0 success
+* @argc: number of commands
+* @argv: array of pointers to commands
+*/
+int main(char argc, char **argv)
+{
+printf("%s\n", argv[0]);
+
+return (0);
+}
+
