940bf03443a879eb674093bf31eb4e0ff8293b4a
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-17 14:08:08 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,10 @@
 * @argc: number of commands
 * @argv: array of pointers to commands
 */
-int main(int argc, char **argv)
+int main(int argc, char const *argv[])
 {
+(void)argc;
 printf("%s\n", argv[0]);
-
 return (0);
 }
 
