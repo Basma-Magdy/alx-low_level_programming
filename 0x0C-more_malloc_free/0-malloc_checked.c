e05d538c0d474432b29c4038c40c984d8559eb41
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-18 15:25:07 -0700

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* malloc_checked - entry point
+* Return: nothing
+* @b: integer
+*/
+void *malloc_checked(unsigned int b)
+{
+int *ptr;
+
+ptr = (int *)malloc(b);
+
+if (ptr == NULL)
+{
+exit (98);
+}
+
+}
+
