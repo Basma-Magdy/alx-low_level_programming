be26713810701f293689507d9c7de505ced9034e
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-18 16:55:41 -0700

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _calloc - entry point
+* Return: always 0 success
+* @nmemb: number of commands
+* @size: array of pointers to commands
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int *ptr;
+ptr = calloc(nmemb, size);
+
+if (nmemb == 0 || size == 0 || ptr == NULL)
+return (NULL);
+
+return(ptr);
+}
