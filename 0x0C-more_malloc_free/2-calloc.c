23186810cd98006b859ca0ba266c3c5d87b40c58
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-18 17:00:18 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int *ptr;
-ptr = malloc(nmemb * size);
+ptr = calloc(nmemb, size);
 
 if (nmemb == 0 || size == 0 || ptr == NULL)
 return (NULL);
