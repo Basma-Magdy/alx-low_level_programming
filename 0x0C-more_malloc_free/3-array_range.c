cfbbaa05bf9c6eed958ab5381c05880135fe6d04
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-19 08:10:37 -0700

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* array_range - entry point
+* Return: always 0 success
+* @min: number of commands
+* @max: array of pointers to commands
+*/
+int *array_range(int min, int max)
+{
+int i, arrsize;
+int *arrptr;
+
+arrsize = max - min + 1;
+arrptr = malloc(arrsize * sizeof(int));
+
+if (min > max || arrptr == 0)
+return (NULL);
+
+
+for (i = 0; i < arrsize; i++)
+{
+arrptr[i] = min;
+min++;
+}
+
+return (arrptr);
+}
