b7abef4d96cbe55b43808346badf3888ef0aa0e4
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-20 04:55:38 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,15 +12,17 @@
 int main(int argc, char *argv[])
 {
 int mul, num1, num2;
-char *p1, *p2;
+/*char *p1, *p2;
 errno = 0;
-
+*/
 if (argc != 3)
 {
 printf("Error\n");
 exit(98);
 }
-
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+/*
 num1 = strtol(argv[1], &p1, 10);
 num2 = strtol(argv[2], &p2, 10);
 
@@ -31,8 +33,9 @@ exit(98);
 }
 else
 {
+*/
 mul = num1 *num2;
 printf("%d\n", mul);
-}
+/*}*/
 return (0);
 }
