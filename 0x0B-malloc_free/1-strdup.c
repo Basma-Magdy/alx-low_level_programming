5639fd5d2a910611b254a4ae877e300c9d665288
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-20 13:33:07 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
-* create_array - entry point
+#include <string.h>
+/**
+* _strdup - entry point
 * Return: always 0 success
-* @size: number of commands
-* @c: array of pointers to comman
+* @str: number of commands
 */
+char *_strdup(char *str)
+{
+int i, size;
+char *str_new;
+
+size = 1;
+
+if (str == NULL)
+return (NULL);
+
+for (i = 0; i < (int)strlen(str); i++)
+size++;
+
+str_new = malloc(size *sizeof(char));
+
+if (str_new == NULL)
+return (NULL);
+
+str_new = str;
+return (str_new);
+}
