51e263a1fcf9c3da972af7b247af3b39ec773f10
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-22 12:05:49 -0700

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+* new_dog - entry point
+* Return: always 0 success
+* @age: structure member
+* @name: structure member
+* @owner: structure member
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *d1;
+d1 = malloc(12);
+d1->name = name;
+d1->age = age;
+d1->owner = owner;
+return (d1);
+}
+
