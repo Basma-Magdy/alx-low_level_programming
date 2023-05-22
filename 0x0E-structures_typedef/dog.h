725aef86068c2e836e3a4e0611788f8d423020a0
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-22 11:08:13 -0700

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifdef DOG_H
+#define DOG_H
+
+struct dog{
+char *name;
+float age;
+char *owner
+};
+
+typedef stuct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
