7c9389ecd6f82904b0d1e03b3174cdf9a5b6f33c
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-23 01:53:29 -0700

    0x0B. C - malloc, free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* argstostr - entry point
+* Return: always 0 success
+* @ac: number of commands
+* @av: array of pointers to comman
+*/
+char *argstostr(int ac, char **av)
+{
+char *arr;
+int i, j, count, word;
+word = 0;
+
+if (ac == 0 || av == NULL)
+return (NULL);
+
+for (i = 0; i < ac; i++)
+count += strlen(av[i]);
+
+arr = malloc(sizeof(char) * count + 1);
+if (arr == 0)
+return (NULL);
+
+for (i = 0; i < ac; i++)
+{
+	for (j = 0; av[i][j] != '\0'; j++)
+	{
+	arr[word] = av[i][j];
+	word++;
+	}
+arr[word] = '\n';
+word++;
+}
+
+arr[word] = '\0';
+return (arr);
+}
