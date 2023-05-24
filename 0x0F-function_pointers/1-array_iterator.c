dd7bb4682ae21f5dfc317cd3b334f0f0d9e6fbce
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-24 07:59:35 -0700

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* array_iterator - entry point
+* Return: always 0 success
+* @array: number of commands
+* @size: number of commands
+* @action: number of commands
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+}
