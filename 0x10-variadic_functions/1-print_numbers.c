df1362dfcde315fb2b6f98284f4756a5e51c288e
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-25 05:32:14 -0700

    0x10. C - Variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+* print_numbers - entry point
+* Return: always 0 success
+* @separator: number of commands
+* @n: array of pointers to commands
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list ptr;
+unsigned int i;
+
+if (n > 0)
+{
+va_start(ptr, n);
+
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(ptr, int));
+
+if (separator && (i < (n - 1)))
+printf("%s", separator);
+
+}
+printf("\n");
+}
+}
