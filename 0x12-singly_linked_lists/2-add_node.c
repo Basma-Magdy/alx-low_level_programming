f7f3818bf1f1ef2615fe32480bda26de2371d6ae
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-06-01 05:00:15 -0700

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+* add_node - entry point
+* Return: size of list
+* @head: pointer to the list
+*@str: string.
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *NewNode;
+NewNode = malloc(sizeof(list_t));
+
+if (!head || !NewNode)
+return (NULL);
+
+if (str)
+{
+NewNode->str = strdup(str);
+
+if (!NewNode->str)
+{
+free(NewNode);
+return (NULL);
+}
+NewNode->len = strlen(str);
+}
+
+NewNode->next = *head;
+*head = NewNode;
+
+return (NewNode);
+}
+
---
+++ 0x12-singly_linked_lists/_strlen.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* _strlen - string length
+* @s: input string
+* Return: integer length
+*/
+unsigned int _strlen(const char *s)
+{
+unsigned int i, len;
+len = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+len ++;
+}
+return (len);
+}
+
