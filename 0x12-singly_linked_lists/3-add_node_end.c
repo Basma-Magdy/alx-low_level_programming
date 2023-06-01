e1d2520ab049afe1f0eaaf2feac3cb48354c5f2d
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-06-01 13:31:40 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,6 +4,23 @@
 #include "lists.h"
 
 /**
+* _strlen - string length
+* @s: input string
+* Return: integer length
+*/
+unsigned int _strlen(const char *s)
+{
+unsigned int i, len;
+len = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+len++;
+}
+return (len);
+}
+
+/**
 * add_node_end - entry point
 * Return: size of list
 * @head: pointer to the list
@@ -28,7 +45,7 @@ free(NewNode);
 return (NULL);
 }
 
-NewNode->len = strlen(str);
+NewNode->len = _strlen(str);
 }
 
 NewNode->next = NULL;
