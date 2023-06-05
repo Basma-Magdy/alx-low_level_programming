80648708b4e782aa7a3ae76726c189461155da09
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-06-05 02:28:41 -0700

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_listint2 - entry point
+* Return: nothing
+* @head: pointer to the list
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *temp, *tofreeNode;
+tofreeNode = *head;
+
+if (!head)
+return;
+
+while (tofreeNode)
+{
+temp = tofreeNode->next;
+free(tofreeNode);
+tofreeNode = temp;
+}
+
+*head = NULL;
+}
