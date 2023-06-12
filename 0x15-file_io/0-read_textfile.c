39e9688971b97aed390a50818c978e8759d99c57
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-06-12 13:53:22 -0700

    0x15. C - File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* read_textfile - entry function
+* @filename: input file
+* @letters: data to be read
+* Return: Number of read bytes
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fdata;
+ssize_t nobytes;
+char buffer[1024 * 8];
+
+if (!filename || !letters)
+return (0);
+
+fdata = open(filename, O_RDONLY);
+
+if (fdata == -1)
+return (0);
+
+nobytes = read(fdata, &buffer[0], letters);
+nobytes = write (STDOUT_FILENO, &buffer[0], nobytes);
+
+if(nobytes == -1)
+return (0);
+
+return (nobytes);
+
+}
