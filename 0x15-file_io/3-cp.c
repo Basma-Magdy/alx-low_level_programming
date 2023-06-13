083a8e84d5678e9ac1d0f8a63a4a70801ea9c9db
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-06-13 13:24:28 -0700

    0x15. C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,24 +16,21 @@ if (ac != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97); }
 from_file = open(av[1], O_RDONLY);
-if (from_file < 0)
+if (from_file == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 exit(98); }
 to_file = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (to_file < 0)
+if (to_file == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-/*close(from_file);*/
 exit(99); }
 
 while ((ab = read(from_file, buffer, 1024)) > 0)
-{
-if (ab < 0)
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 if ((write(to_file, buffer, ab)) != ab)
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-}
+if (ab < 0)
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 from_file = close(from_file);
 to_file = close(to_file);
 if (from_file)
